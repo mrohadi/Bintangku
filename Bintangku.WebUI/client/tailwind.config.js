3d27aad1922acbf8fcdf62037041583df664d669
teguh <teguh@elgibor-solution.com>
2021-06-03 18:37:21 +0700

    update: angular 12 add tailwind ui

---
+++ Bintangku.WebUI/client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.html',
+      './src/**/*.scss'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
