1c1446bcdea31bcd1a29303437c90b1ce0a72119
teguh tri nugroho <ttrinugroho@gmail.com>
2021-06-05 09:34:33 +0700

    ubah tampilan home componen

--- Bintangku.WebUI/client/src/app/app.component.ts
+++ Bintangku.WebUI/client/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { AccountServices } from './_services/account.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'Bintangku';
